82f82d4fbb8c6e5d75cc23f93bd065e9f5868a45
Sukhi898 <146666624+Sukhi898@users.noreply.github.com>
2024-08-02 00:52:47 +0530

    Update Form.js

--- src/Controllers/Form.js
+++ src/Controllers/Form.js
@@ -11,7 +11,7 @@ const createForm = async (req, res) => {
     }
 
     const uniqueId = uuidv4();
-    const uniqueLink = `http://localhost:5174/form/unique/${uniqueId}`;
+    const uniqueLink = `https://form-builder-bot.netlify.app//form/unique/${uniqueId}`;
 
     const form = new Form({
       formName,
